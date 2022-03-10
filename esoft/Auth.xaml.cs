f4dcb157af6c16d6d420db046aa5abb5ed7b3f15
Dany <zqxw1245@mail.ru>
2022-03-10 14:26:36 +0300

    first commit

---
+++ esoft/Auth.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace esoft
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).ButtonAuth.Content = "Выход";
+                }
+            }
+        }
+
+        private void clickButtonLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clickButtonLogout(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ esoft/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace esoft
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.NavigationService.Navigate(new Uri("Auth.xaml", UriKind.Relative));
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void AuthClick(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            //MainFrame.NavigationService.Navigate(new Uri("AuthWindow.xaml", UriKind.Relative));
+            if (clickedButton.Content == "Выход")
+            {
+                clickedButton.Content = "Войти";
+                MainFrame.Content = null;
+            }
+            else
+            {
+                MainFrame.NavigationService.Navigate(new Uri("Auth.xaml", UriKind.Relative));
+            }
+        }
+
+    }
+}
